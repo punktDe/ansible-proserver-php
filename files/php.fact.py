cadc6ca173ae5aa7ba4f9e7d54413ec34d9c1144
Maik Peuser <peuser@punkt.de>
2025-07-14 15:35:56 +0200

    use default php to determine version
    on debian with apt-cache always the latest possible version is returned, even if a lower version is installed

--- files/php.fact.py
+++ files/php.fact.py
@@ -29,11 +29,7 @@ class PhpFacts:
             raise OSError(f"Unsupported OS family: {os_family}")
 
     def get_php_version(self) -> str:
-        os_family = self.get_os_family()
-        if os_family == "debian":
-            command='apt-cache search --names-only "^php[0-9].[0-9]$" | grep -o "[0-9]\\.[0-9]" | sort -V | tail -n 1'
-        else:
-            command="php -r 'echo phpversion();' | grep -o '[0-9]\\.[0-9]' | head -n1"
+        command="php -r 'echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;'"
         command_output = subprocess.run(command, shell=True, capture_output=True)
         clean_output = command_output.stdout.decode().strip()
         if re.match(r"[0-9]+\.[0-9]", clean_output):
